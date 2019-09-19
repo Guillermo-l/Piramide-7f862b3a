aa698d19c80d1bd55aa3091e8b6907cc6ab54805
Guillermo Latumahina <glatumahina@roc-dev.com>
2019-09-19 14:42:27 +0200

    Uitwerking van de exercise

--- piramide.php
+++ piramide.php
@@ -5,9 +5,10 @@ echo "Hoeveel stapels wil je hebben?".PHP_EOL;
 $stapels = readline(">");
 
 for($i = 0; $i < $stapels; $i++){
-    for($j = 1; $j <1 $i; $j++){
-        echo "*";
-    }
+    for($j = 0; $j <= $i; $j++){
+    echo "*";
+}
+
     echo PHP_EOL;
 }
 
